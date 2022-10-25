bf79ec42cae7e7265d5713d5ba0aa035a313b8bf
Yohan <dladygks506@gmail.com>
2022-10-25 15:44:09 +0900

    mariadb pr test

--- src/test/java/com/fittoo/utills/CalendarUtilTest.java
+++ src/test/java/com/fittoo/utills/CalendarUtilTest.java
@@ -1,33 +1,33 @@
-package com.fittoo.utills;
-
-import com.fittoo.member.model.MemberDto;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.util.Calendar;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-//@SpringBootTest
-//@Transactional
-class CalendarUtilTest {
-
-    @Test
-    void calendarTest() {
-        //given
-        Calendar cal = Calendar.getInstance();
-        cal.set(2022, 9, 21); // month = 0부터
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        System.out.println("dayOfWeek = " + dayOfWeek); // 요일
-        System.out.println(cal.getActualMaximum(Calendar.DAY_OF_MONTH)); // 말일
-        //when
-        System.out.println(LocalDate.now().getMonth());
-        System.out.println(LocalDate.now().getMonthValue());
-        //then
-    }
-
+//package com.fittoo.utills;
+//
+//import com.fittoo.member.model.MemberDto;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.LocalDate;
+//import java.util.Calendar;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+////@SpringBootTest
+////@Transactional
+//class CalendarUtilTest {
+//
+//    @Test
+//    void calendarTest() {
+//        //given
+//        Calendar cal = Calendar.getInstance();
+//        cal.set(2022, 9, 21); // month = 0부터
+//        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+//        System.out.println("dayOfWeek = " + dayOfWeek); // 요일
+//        System.out.println(cal.getActualMaximum(Calendar.DAY_OF_MONTH)); // 말일
+//        //when
+//        System.out.println(LocalDate.now().getMonth());
+//        System.out.println(LocalDate.now().getMonthValue());
+//        //then
+//    }
+//
 //    @Test
 //    void getCalendarTest() {
 //        //given
@@ -36,5 +36,5 @@ class CalendarUtilTest {
 //        System.out.println("dayOfOneDay = " + dayOfOneDay);
 //        System.out.println("lastDayOfMonth = " + lastDayOfMonth);
 //    }
-
-}
+//
+//}
