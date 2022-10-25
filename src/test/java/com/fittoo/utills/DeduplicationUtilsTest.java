7b82831d6fa6eb925998aefa1c487d0d7d909b2c
Yohan <dladygks506@gmail.com>
2022-10-25 15:32:15 +0900

    mariadb pr test

--- src/test/java/com/fittoo/utills/DeduplicationUtilsTest.java
+++ src/test/java/com/fittoo/utills/DeduplicationUtilsTest.java
@@ -16,8 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-//@SpringBootTest
-//@Transactional
+@SpringBootTest
+@Transactional
 class DeduplicationUtilsTest {
 
 	@Autowired
